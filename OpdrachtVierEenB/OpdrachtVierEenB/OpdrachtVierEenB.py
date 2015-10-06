cb4f7dc3027642986eb52fe1919dfcbec5753d52
ahmadhawler <ahmaddhawlery94@gmail.com>
2015-10-06 23:07:48 +0200

    OpdrachtVierEenB compleet2

--- OpdrachtVierEenB/OpdrachtVierEenB/OpdrachtVierEenB.py
+++ OpdrachtVierEenB/OpdrachtVierEenB/OpdrachtVierEenB.py
@@ -2,7 +2,7 @@
 
 if celsius > -273 and celsius < 141: #If it's between minus 273 and plus 141 it can be possible
     kelvin = celsius + 273 #Here it will define what the output is
-    print "That is %s in Kelvin" %(kelvin) #It will print the text
+    print "That is" , kelvin ,  "in Kelvin" #It will print the text
 
 else:
     print "That's not possible user, you're going to die" #So if the imput isn't possible, it will print out this message
