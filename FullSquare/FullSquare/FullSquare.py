5c3ef385d9a8c71273f8ddcdf0cdb9e3cc490734
ahmadhawler <ahmaddhawlery94@gmail.com>
2015-10-06 18:07:41 +0200

    Full Square opdracht

---
+++ FullSquare/FullSquare/FullSquare.py
@@ -0,0 +1,12 @@
+a = int(raw_input("How big do you want the full square to be?\n")) #It will ask the user how many astericks
+#The 'a' is an integer wich the user will be asked to fill in. A has an value but we still don't know what value
+
+
+
+x, y = a, a #Here, the x line will be an value, and the y line will be an value in an range
+
+
+for i in range (x): #So for how many astericks, there is an value wich will be an input for the user called x
+    for j in range (y): #So for how many astericks, there is an value wich will be an input from the user called y
+        print '*', #It will print the astrick
+    print #Eventually, it will print everything out as said
