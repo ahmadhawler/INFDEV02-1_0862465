ee434b265d3b6c206b875820477fb54a19f9a6ca
ahmadhawler <ahmaddhawlery94@gmail.com>
2015-10-06 23:04:33 +0200

    OpdrachtVierEenA met commentaar

--- OpdrachtVierEenA/OpdrachtVierEenA/OpdrachtVierEenA.py
+++ OpdrachtVierEenA/OpdrachtVierEenA/OpdrachtVierEenA.py
@@ -1,3 +1,4 @@
-celsius = float(input('What is the Celsius temperature?'))
-fahrenheit = 9.0 / 5.0 * celsius + 32
-print ('The temperature is' , fahrenheit, 'degrees fahrenheit' )
+﻿celsius = float(input('What is the Celsius temperature?')) #Here it will ask for the user to give the temperature in celsius,
+                                                           #with an floating point number
+fahrenheit = 9.0 / 5.0 * celsius + 32                      #Here it will calculate the fahrenheit from the input
+print "The temperature is", fahrenheit , "degrees fahrenheit" #Eventually it wil print out the value plus the fahrenheit.
